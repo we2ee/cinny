81ca9eecc7554ed6d66be887b3b492eb0b4f2260
sanj <67624670+iodrift@users.noreply.github.com>
2025-03-07 15:07:59 -0800

    Apple We2ee branding across app

--- src/app/pages/auth/AuthFooter.tsx
+++ src/app/pages/auth/AuthFooter.tsx
@@ -5,20 +5,14 @@ import * as css from './styles.css';
 export function AuthFooter() {
   return (
     <Box className={css.AuthFooter} justifyContent="Center" gap="400" wrap="Wrap">
-      <Text as="a" size="T300" href="https://cinny.in" target="_blank" rel="noreferrer">
+      <Text as="a" size="T300" href="https://we2.ee/about" target="_blank" rel="noreferrer">
         About
       </Text>
-      <Text
-        as="a"
-        size="T300"
-        href="https://github.com/ajbura/cinny/releases"
-        target="_blank"
-        rel="noreferrer"
-      >
-        v4.5.1
+      <Text as="a" size="T300" href="https://we2.ee/terms" target="_blank" rel="noreferrer">
+        Terms
       </Text>
-      <Text as="a" size="T300" href="https://twitter.com/cinnyapp" target="_blank" rel="noreferrer">
-        Twitter
+      <Text as="a" size="T300" href="https://we2.ee/privacy" target="_blank" rel="noreferrer">
+        Privacy Policy
       </Text>
       <Text as="a" size="T300" href="https://matrix.org" target="_blank" rel="noreferrer">
         Powered by Matrix
